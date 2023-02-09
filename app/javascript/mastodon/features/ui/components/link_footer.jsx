ac38afe1ee9c809d047fd3bd4bf4d9f10c05e2d0
Isa S <smiba@users.noreply.github.com>
2023-02-09 21:42:46 +0100

    Include link to elk interface

--- app/javascript/mastodon/features/ui/components/link_footer.jsx
+++ app/javascript/mastodon/features/ui/components/link_footer.jsx
@@ -70,7 +70,9 @@ class LinkFooter extends PureComponent {
             </>
           )}
           {DividingCircle}
-          <Link to='/privacy-policy' target={multiColumn ? '_blank' : undefined}><FormattedMessage id='footer.privacy_policy' defaultMessage='Privacy policy' /></Link>
+          <Link to='/privacy-policy'><FormattedMessage id='footer.privacy_policy' defaultMessage='Privacy policy' /></Link>
+          {DividingCircle}
+          <a href='https://elk.woof.tech'><FormattedMessage id='footer.elk' defaultMessage='Elk (Alpha)' /></a>
         </p>
 
         <p>

4a127137c4ff50ac9a0b9a73f7a613d7270f405d
Isa S <smiba@users.noreply.github.com>
2023-02-09 21:42:46 +0100

    Include link to elk interface

--- app/javascript/mastodon/features/ui/components/link_footer.js
+++ app/javascript/mastodon/features/ui/components/link_footer.js
@@ -80,6 +80,8 @@ class LinkFooter extends React.PureComponent {
           )}
           {DividingCircle}
           <Link to='/privacy-policy'><FormattedMessage id='footer.privacy_policy' defaultMessage='Privacy policy' /></Link>
+          {DividingCircle}
+          <a href='https://elk.woof.tech'><FormattedMessage id='footer.elk' defaultMessage='Elk (Alpha)' /></a>
         </p>
 
         <p>

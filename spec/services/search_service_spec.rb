a0dac7da41ab5ac33eaacf8d4b95a495ff7e6c43
Vyr Cossont <VyrCossont@users.noreply.github.com>
2023-04-05 11:24:44 -0700

    Extended post and account search

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -88,15 +88,7 @@ describe SearchService, type: :service do
           allow(Tag).to receive(:search_for)
 
           results = subject.call(query, nil, 10)
-          expect(Tag).not_to have_received(:search_for)
-          expect(results).to eq empty_results
-        end
-        it 'does not include account when starts with # character' do
-          query = '#tag'
-          allow(AccountSearchService).to receive(:new)
-
-          results = subject.call(query, nil, 10)
-          expect(AccountSearchService).to_not have_received(:new)
+          expect(Tag).to_not have_received(:search_for)
           expect(results).to eq empty_results
         end
       end
